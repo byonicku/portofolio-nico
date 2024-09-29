9a2b9e54fb7e1dba0c8934dcbffe94f67fdbb588
Nico Herlim <nicoherlim2003@gmail.com>
2024-09-29 14:27:00 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Typography>WIP :D</Typography>
+      </Box>
+    </>
+  );
+}
+
+export default App;
